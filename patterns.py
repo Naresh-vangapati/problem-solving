71b8416d80dbe40049855e290118afc859bf4df6
Naresh-vangapati <nareshvangapati1999@gmail.com>
2025-03-22 16:00:23 +0530

    patterns

---
+++ patterns.py
@@ -0,0 +1,96 @@
+# * * * * * print this pattern
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         print('*', end=' ')
+#     print()     
+
+
+# # or
+
+# # print a right angle triangle
+
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         if i >= j:
+#             print('*', end=' ')
+#     print()    
+
+
+#print upper right angle triangle  
+
+
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         if i <= j:
+#             print('*', end=' ')
+#         else:
+#             print(' ', end= ' ')    
+#     print()    
+
+
+# print a pyramid shape
+
+# a =6
+# for i in range(0,a-1):
+#     for j in range(0,a):
+#         if i >= j:
+#             print('*', end=' ')
+#     print()    
+# for i in range(0,a-1):
+#     for j in range(0,a):
+#         if i <= j:
+#             print('*', end=' ')
+#     print()    
+
+# print boundary elements in a square
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         if i in [0, a-1] or j in [0,a-1] or i + j == a - 1 or i == j:
+#             print('*', end=' ')
+#         else:
+#             print(' ', end=' ')    
+#     print()     
+
+
+# *         * 
+# * *     * * 
+# *   * *   *       this pattern code
+# *   * *   *
+# * *     * *
+# *         *
+
+
+
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         if j in [0,a-1] or i + j == a - 1 or i == j:
+#             print('*', end=' ')
+#         else:
+#             print(' ', end=' ')    
+#     print()         
+
+
+
+
+# * * * * * * 
+#   *     *   
+#     * *         this pattern code
+#     * *
+#   *     *
+# * * * * * *
+
+
+
+# a =6
+# for i in range(0,a):
+#     for j in range(0,a):
+#         if i in [0, a-1] or i + j == a - 1 or i == j:
+#             print('*', end=' ')
+#         else:
+#             print(' ', end=' ')    
+#     print()     
